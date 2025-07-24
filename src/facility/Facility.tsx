52101fb9da8a1024fa65990aa92bc72424478ca9
shivtcs1983 <shivtcs1983@gmail.com>
2025-07-24 14:22:59 -0400

    clean-code

--- src/facility/Facility.tsx
+++ src/facility/Facility.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-//import ImageSlider from './../components/ImageSlider'
-//import QuestionUI from './../components/QuestionUI';
 import CopyRenameImages from './../components/RenameImages';
 import ImageOCRApp from './../components/ImageOCRApp';
 import FolderOCRApp from './../components/FolderOCRApp';
-import QuestionForm from './../components/QuestionForm';
-import  FileDiffViewer  from './../filedifference/FileDiffViewer';
-const Facility = ({ }) => {
+import FileDiffViewer from './../filedifference/FileDiffViewer';
+import './Facility.scss'; // Add this for styling
 
-    return (<div>
-        <p> This facility page</p>
+const Facility = () => {
+  return (
+    <div className="facility-page">
+      <h1 className="page-title">Facility Page</h1>
+      <section className="facility-section">
+        <h2 className="section-title">Image OCR</h2>
+        <ImageOCRApp />
+      </section>
 
-        <h1 className="title">Gallery</h1>
-        {/* <ImageSlider /> */}
-        {/* <QuestionUI/> */}
-        <CopyRenameImages/>
-        <ImageOCRApp/>
-        <FolderOCRApp/>
-           <FileDiffViewer/>
-          <QuestionForm />
-       
-    </div>)
-}
+      <section className="facility-section">
+        <h2 className="section-title">Folder OCR</h2>
+        <FolderOCRApp />
+      </section>
+
+      <section className="facility-section">
+        <h2 className="section-title">File Difference Viewer</h2>
+        <FileDiffViewer />
+      </section>
+      
+      <section className="facility-section">
+        <h2 className="section-title">Copy & Rename Images</h2>
+        <CopyRenameImages />
+      </section>
+    </div>
+  );
+};
 
 export default Facility;
